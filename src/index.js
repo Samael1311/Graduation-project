ab0943455a6f06be75426a2ce3e5aa419b79a534
Rimma <teststudy1311@gmail.com>
2019-03-30 15:44:42 +0600

    Showup

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ window.addEventListener('DOMContentLoaded', ()=>{
 	'use strict';
 
 	let indexSlider = require('./parts/slider_index'),
-			play = require('./parts/play_index');
+			play = require('./parts/play_index'),
+			showupSlider = require('./parts/showup_slider');
 
 
 	indexSlider();
 	play();
+	showupSlider();
 
 
 
---
+++ src/parts/showup_slider.js
@@ -0,0 +1,51 @@
+function showupSlider() {
+
+	let showupSliders = document.querySelectorAll('.showup__content-slider a'),
+		boxContent = document.querySelector('.showup__content-slider'),
+		btnRight = document.querySelector('.right'),
+		btnLeft = document.querySelector('.left');
+
+
+	boxContent.style.display = '-webkit-inline-box';
+	boxContent.style.overflow = 'hidden';
+	let slideIndex = 1;
+
+	btnRight.addEventListener('click', () => {
+		let active = document.querySelector('.showup__content-slider a.card-active');
+		boxContent.appendChild(active);
+		plusSlides(1);
+	});
+	btnLeft.addEventListener('click', () => {
+		let active = boxContent.lastChild;
+		console.log(active);
+		boxContent.insertBefore(active, boxContent.firstChild);
+		plusSlides(-1);
+	});
+	
+	function moveSlides(n){
+
+		if(n > showupSliders.length){
+			slideIndex = 1;
+		}
+		if(n < 1){
+			slideIndex = showupSliders.length;
+		}
+		for (let i = 0; i < showupSliders.length; i++) {
+			showupSliders[i].classList.remove('card-active');
+			showupSliders[i].children[1].style.opacity = '0.4';
+			
+		}
+		showupSliders[slideIndex-1].classList.add('card-active');
+		showupSliders[slideIndex-1].children[1].style.opacity = '1';
+	}
+
+
+	function plusSlides(n){
+
+		moveSlides(slideIndex += n);
+	
+	}
+	moveSlides(slideIndex);
+}
+
+module.exports = showupSlider;
